60cf475d3a0d813565815819a72a724a5823b8de
ShayanHarati <shayanharati8@gmail.com>
2024-04-07 21:23:18 +0330

    seprator added

---
+++ PajoPhone/wwwroot/js/MoneySeprator.js
@@ -0,0 +1,34 @@
+function reformat(){
+    ans = []
+    if($("#demo").val()!=""){
+        var number = $("#demo").val().replaceAll(",","")
+        var number_cop= number
+        while(number>=1000){
+            if (number%1000==0){
+                ans.push("000")
+            }else if(number%1000<10 && number_cop>=1000){
+                ans.push("00"+number%1000)
+            }else if(number%1000>=10 && number%1000<100&& number_cop>=1000){
+                ans.push("0"+number%1000)
+            }else{
+                ans.push(number%1000)
+            }
+
+            number = (number-(number%1000))/1000
+
+        }
+        ans.push(number%1000)
+        ans.reverse()
+
+        console.log(ans.join());
+        $("#demo").val(ans.join(","))
+    }
+}
+$("#demo").keypress((e)=>{
+    reformat()
+
+})
+$("#demo").change((e)=>{
+    reformat()
+
+})
